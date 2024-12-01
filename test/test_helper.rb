efc297f940124791051ff78b024887ca60434b33
Georg Ledermann <georg@ledermann.dev>
2024-12-01 14:06:43 +0100

    Ensure Measurement is not empty
    Belongs to #233

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,13 @@ SimpleCov.start
 require 'minitest/autorun'
 
 require File.expand_path './support/vcr_setup.rb', __dir__
+
+def mock_env(partial_env_hash)
+  old = ENV.to_hash
+  ENV.update partial_env_hash
+  begin
+    yield
+  ensure
+    ENV.replace old
+  end
+end
